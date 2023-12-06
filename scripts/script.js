34fe95c648cb8ec1e2ac58aa8bb53a5f63a6a2d8
levent <levent1ozgur+github@protonmail.com>
2023-12-06 15:39:52 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,24 @@
-        function showSelectedOptions() {
-            const form = document.getElementById('optionsForm');
-            const selectedOptionsDiv = document.getElementById('selectedOptions');
-            const selectedOptions = [];
+  document.addEventListener('DOMContentLoaded', function () {
+            const ageFieldset = document.getElementById('ageFieldset');
+            const expressionsFieldset = document.getElementById('expressionsFieldset');
+            const generateButton = document.getElementById('generateButton');
 
-            // Loop through checkboxes to find selected options
-            for (let i = 0; i < form.elements.length; i++) {
-                const element = form.elements[i];
+            // Add event listener to age radio buttons
+            const ageRadios = document.querySelectorAll('input[name="age"]');
+            ageRadios.forEach(function (radio) {
+                radio.addEventListener('change', function () {
+                    if (this.checked) {
+                        // Hide age fieldset, show expressions fieldset, and show generate button
+                        ageFieldset.classList.add('hidden');
+                        expressionsFieldset.classList.remove('hidden');
+                        generateButton.classList.remove('hidden');
+                    }
+                });
+            });
 
-                if (element.type === 'checkbox' && element.checked) {
-                    selectedOptions.push(element.name);
-                }
-            }
-
-            // geenerate prompts
-            selectedOptionsDiv.innerHTML = `<p>Selected Options: ${selectedOptions.join(', ')}</p>`;
-        }
-  
+            // Add event listener to generate button
+            generateButton.addEventListener('click', function () {
+                // Replace this with your actual generate logic
+                alert('Generated: ' + document.querySelector('input[name="age"]:checked').value + ', ' + document.querySelector('input[name="expressions"]:checked').value);
+            });
+        });
