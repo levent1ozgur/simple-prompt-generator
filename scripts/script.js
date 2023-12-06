5e8d40e0c2a92117c7be67a2f7cf43a4eea381cb
levent <levent1ozgur+github@protonmail.com>
2023-12-06 16:06:59 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,63 @@
-  document.addEventListener('DOMContentLoaded', function () {
-            const ageFieldset = document.getElementById('ageFieldset');
-            const expressionsFieldset = document.getElementById('expressionsFieldset');
-            const generateButton = document.getElementById('generateButton');
+document.addEventListener('DOMContentLoaded', function () {
+    const fieldsets = document.querySelectorAll('fieldset');
+    const generateButton = document.getElementById('generateButton');
 
-            // Add event listener to age radio buttons
-            const ageRadios = document.querySelectorAll('input[name="age"]');
-            ageRadios.forEach(function (radio) {
-                radio.addEventListener('change', function () {
-                    if (this.checked) {
-                        // Hide age fieldset, show expressions fieldset, and show generate button
-                        ageFieldset.classList.add('hidden');
-                        expressionsFieldset.classList.remove('hidden');
+    // Add event listeners to all radio buttons in all fieldsets
+    fieldsets.forEach(function (fieldset) {
+        const radios = fieldset.querySelectorAll('input[type="radio"]');
+        radios.forEach(function (radio) {
+            radio.addEventListener('change', function () {
+                if (this.checked) {
+                    // Hide the current fieldset
+                    fieldset.classList.add('hidden');
+
+                    // Show the next fieldset (if any)
+                    const nextFieldset = getNextFieldset(fieldset);
+                    if (nextFieldset) {
+                        nextFieldset.classList.remove('hidden');
+                    }
+
+                    // Show the generate button if it's the last fieldset
+                    if (!nextFieldset) {
                         generateButton.classList.remove('hidden');
                     }
-                });
-            });
-
-            // Add event listener to generate button
-            generateButton.addEventListener('click', function () {
-                // Replace this with your actual generate logic
-                alert('Generated: ' + document.querySelector('input[name="age"]:checked').value + ', ' + document.querySelector('input[name="expressions"]:checked').value);
+                }
             });
         });
+    });
+
+    // Add event listener to generate button
+    generateButton.addEventListener('click', function () {
+        const selectedValues = getSelectedValues();
+        let generatedMessage = 'Generated:';
+
+        for (const fieldsetId in selectedValues) {
+            generatedMessage += ` ${selectedValues[fieldsetId]},`;
+        }
+
+        // Remove the trailing comma and display the message
+        alert(generatedMessage.slice(0, -1));
+    });
+
+    // Helper function to get the next visible fieldset
+    function getNextFieldset(currentFieldset) {
+        for (let i = 0; i < fieldsets.length - 1; i++) {
+            if (fieldsets[i] === currentFieldset) {
+                return fieldsets[i + 1];
+            }
+        }
+        return null;
+    }
+
+    // Helper function to get the selected values from all fieldsets
+    function getSelectedValues() {
+        const selectedValues = {};
+        fieldsets.forEach(function (fieldset) {
+            const checkedRadio = fieldset.querySelector('input[type="radio"]:checked');
+            if (checkedRadio) {
+                selectedValues[fieldset.id] = checkedRadio.value;
+            }
+        });
+        return selectedValues;
+    }
+});
